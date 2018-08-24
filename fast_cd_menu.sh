17b0de9c4cc4f34c95c637140c6a050651ccbf1a
xhawk18 <xhawk18@gmail.com>
2018-08-24 20:58:16 +0800

    optimize behaviour after press ESC.

--- fast_cd_menu.sh
+++ fast_cd_menu.sh
@@ -592,6 +592,7 @@ function _cd_list() {
                     changed=1
                     break
                 elif (( cd_entry != -1 )); then
+                    echo -e '\r';
                     break
                 fi
             done
